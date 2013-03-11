f98ccb72d6169e2368f483e94153c08a531e95d3
zhangwei <zhangweihust@gmail.com>
2013-03-11 09:03:10 +0800

    update

--- src/com/zhangwei/speakloudly/fragment/PhoneNumFragment.java
+++ src/com/zhangwei/speakloudly/fragment/PhoneNumFragment.java
@@ -25,10 +25,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class PhoneNumFragment  extends Fragment {
+public class PhoneNumFragment extends Fragment {
 	MscActivity mActivity;
-	
-	//UI components
+
+	// UI components
 	private EditText mEdit;
 	private TextView mError;
 	private Button mNext;
@@ -36,7 +36,6 @@ public class PhoneNumFragment  extends Fragment {
 
 	public PhoneNumFragment() {
 	}
-
 
 	// Called once the Fragment has been created in order for it to
 	// create its user interface.
@@ -46,124 +45,138 @@ public class PhoneNumFragment  extends Fragment {
 		RuntimeLog.log("PhoneNumFragment - onCreateView");
 		// Create, or inflate the Fragment's UI, and return it.
 		// If this Fragment has no UI then return null.
-		View view =  inflater.inflate(R.layout.phonenum_fragment, container, false);
+		View view = inflater.inflate(R.layout.phonenum_fragment, container,
+				false);
 
-		mEdit = (EditText)view.findViewById(R.id.phonenum);
-		mError = (TextView)view.findViewById(R.id.error);
-		mNext = (Button)view.findViewById(R.id.next);
-		
+		mEdit = (EditText) view.findViewById(R.id.phonenum);
+		mError = (TextView) view.findViewById(R.id.error);
+		mNext = (Button) view.findViewById(R.id.next);
+
 		return view;
 	}
-	
+
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState){
+	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		RuntimeLog.log("PhoneNumFragment - onActivityCreated");
-		mNext.setOnClickListener(new OnClickListener(){
+		mNext.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// mSpeakText.setText(text);
 				String phoneNum = mEdit.getText().toString();
-				if(validateNumber(phoneNum)){
-					//seems ok, then verify phone number through unicom server
-					mActivity.mHandler.sendEmptyMessage(MscActivity.VERIFYING_PHONENUM);
-					
-					//set the pull pin time out event
+				if (validateNumber(phoneNum)) {
+					// seems ok, then verify phone number through unicom server
+					mActivity.mHandler
+							.sendEmptyMessage(MscActivity.VERIFYING_PHONENUM);
+
+					// set the pull pin time out event
 					Message msg = new Message();
 					msg.what = MscActivity.STOP_VFY_PHONENUM;
 					msg.obj = "timeout";
-					mActivity.mHandler.sendMessageDelayed(msg, MscActivity.TIMEOUT_DELAY);
-					
-					UnicomClient.performVerfiyNum(null, mActivity.mHandler, new Callback(){
-						public void call(ResponseWrapper resp) {
-							
-								mActivity.handleNetworkError(resp);
-								if(resp!=null && resp.isValid() && resp.isNoError()){
-									String errMsg = ServerSideErrorMsg.getMsg(resp.getStatus());
-									if(errMsg!=null){
-										unLockInput(errMsg);
-									}else{
-										mActivity.mHandler.removeMessages(MscActivity.STOP_VFY_PHONENUM);
-										mActivity.goToPage(MscActivity.SPEAK_LOUDLY_VIEW, true);
+					mActivity.mHandler.sendMessageDelayed(msg,
+							MscActivity.TIMEOUT_DELAY);
+
+					UnicomClient.performVerfiyNum(null, mActivity.mHandler,
+							new Callback() {
+								public void call(ResponseWrapper resp) {
+
+									mActivity.handleNetworkError(resp);
+									// if(resp!=null && resp.isValid() &&
+									// resp.isNoError()){
+									if (true) {
+
+										mActivity.mHandler
+												.removeMessages(MscActivity.STOP_VFY_PHONENUM);
+										mActivity.goToPage(
+												MscActivity.SPEAK_LOUDLY_VIEW,
+												true);
+
+									} else {
+										if (resp != null) {
+											String errMsg = ServerSideErrorMsg
+													.getMsg(resp.getStatus());
+											if (errMsg != null) {
+												unLockInput(errMsg);
+											}
+										}
+										mActivity.mHandler
+												.removeMessages(MscActivity.STOP_VFY_PHONENUM);
+										Message msg = new Message();
+										msg.what = MscActivity.STOP_VFY_PHONENUM;
+										msg.obj = "error";
+										mActivity.mHandler.sendMessageDelayed(
+												msg, MscActivity.RUNTIME_DELAY);
+										// unLockInput(null);
 									}
-								}else{
-									mActivity.mHandler.removeMessages(MscActivity.STOP_VFY_PHONENUM);
-									Message msg = new Message();
-									msg.what = MscActivity.STOP_VFY_PHONENUM;
-									msg.obj = "error";
-									mActivity.mHandler.sendMessageDelayed(msg, MscActivity.RUNTIME_DELAY);
-									//unLockInput(null);
+
 								}
-							
-						}
-					});
-					
-				}else{
+							});
+
+				} else {
 					unLockInput(phoneNum + "不是一个正确的手机号码");
 				}
-				
+
 			}
 		});
 	}
 
-	
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
-		
+
 		RuntimeLog.log("PhoneNumFragment - onAttach");
 
 		try {
-			mActivity =  (MscActivity)activity;
-			//onGotoPageListener = (OnGotoPageListener) activity;
+			mActivity = (MscActivity) activity;
+			// onGotoPageListener = (OnGotoPageListener) activity;
 		} catch (ClassCastException e) {
 			throw new ClassCastException(activity.toString()
 					+ " must implement OnGotoPageListener");
 		}
 	}
 
-
-	
-	public void LockInput(){
+	public void LockInput() {
 		RuntimeLog.log("PhoneNumFragment - LockInput - IN");
 		mNext.setText("");
-		mNext.setClickable(false);			
+		mNext.setClickable(false);
 		mNext.setBackgroundResource(R.anim.verifying_phonenumber);
-		verifyingPhoneNumberAnimation = (AnimationDrawable)mNext.getBackground();
+		verifyingPhoneNumberAnimation = (AnimationDrawable) mNext
+				.getBackground();
 		verifyingPhoneNumberAnimation.start();
-		
+
 		mError.setVisibility(View.GONE);
-		
+
 	}
-	
-	public void unLockInput(String errMsg){
+
+	public void unLockInput(String errMsg) {
 		RuntimeLog.log("PhoneNumFragment - unLockInput - IN, errMsg:" + errMsg);
-		if ((verifyingPhoneNumberAnimation!=null) && verifyingPhoneNumberAnimation.isRunning()){
+		if ((verifyingPhoneNumberAnimation != null)
+				&& verifyingPhoneNumberAnimation.isRunning()) {
 			verifyingPhoneNumberAnimation.stop();
 		}
 		mNext.setText(R.string.next);
 		mNext.setClickable(true);
 		mNext.setBackgroundResource(R.drawable.btn_shape_white_rec);
-		
-		if(errMsg!=null){
+
+		if (errMsg != null) {
 			mError.setText(errMsg);
 			mError.setTextColor(getResources().getColor(R.color.red));
 			mError.setVisibility(View.VISIBLE);
 		}
-		
+
 	}
-	
-	
-	private boolean validateNumber(String number){
-    	//Pattern pattern=Pattern.compile("(^\\+91\\d{10}$|^0091\\d{10}$|^0\\d{10}$|^\\d{10}$)");
-    	//Pattern pattern=Pattern.compile("(^\\d{10}$)");
-    	Pattern pattern=Pattern.compile("(^13\\d{9}$|^15\\d{9}$|^16\\d{9}$|^18\\d{9}$)");
-		Matcher matcher=pattern.matcher(number);
-		if(!matcher.find()){
-			return false; 
+
+	private boolean validateNumber(String number) {
+		// Pattern
+		// pattern=Pattern.compile("(^\\+91\\d{10}$|^0091\\d{10}$|^0\\d{10}$|^\\d{10}$)");
+		// Pattern pattern=Pattern.compile("(^\\d{10}$)");
+		Pattern pattern = Pattern
+				.compile("(^13\\d{9}$|^15\\d{9}$|^16\\d{9}$|^18\\d{9}$)");
+		Matcher matcher = pattern.matcher(number);
+		if (!matcher.find()) {
+			return false;
 		}
 		return true;
 	}
-
 
 }
